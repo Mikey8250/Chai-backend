32f33ad0f0a65b618d4530f5615c876a909ba255
Mikey8250 <mohitburnwal2@gmail.com>
2025-09-22 05:30:05 +0530

    add user and video model

---
+++ src/models/video.models.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import { JsonWebTokenError } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // clodinary URL
+      required: true,
+    },
+    thumbnail: {
+      type: String, // clodinary URL
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // clodinary URL
+      default: 0,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const User = mongoose.model("User", userSchema);
