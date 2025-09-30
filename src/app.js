d2a2e30283d51a31f1bc4ca6ff62f7aa6c5f80c5
Mikey8250 <mohitburnwal2@gmail.com>
2025-09-30 05:50:18 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { LIMIT } from "./constant";
+import { LIMIT } from "./constant.js";
 
 const app = express();
 
@@ -12,5 +12,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: LIMIT }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
+//http://localhost:8000//api/v1/users/register
 
 export { app };
